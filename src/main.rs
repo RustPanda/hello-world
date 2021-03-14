ff5f61ea333551db6ce421db7fed8dd84387f76f
Golubev Matvei <golubevmt@gmail.com>
2021-03-14 15:50:36 +0300

    Добавил пути в actix для создания и чтения данных из sled

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,49 @@
 use actix_web::{
     get,
-    post,
-    delete,
+    // post,
+    // delete,
     web,
     App,
     HttpServer,
-    Responder,
-    HttpResponse,
-    Either,
+    // Responder,
+    // HttpResponse,
+    // Either,
     middleware,
+    Result
 };
 use sled;
 
+#[get("/{key}")]
+async fn get(data: web::Data<sled::Db>, web::Path((key,)): web::Path<(String,)>) -> Result<String> {
+    match  data.get(key.as_bytes()).unwrap() {
+        Some(value) => Ok(format!("Value: {}", String::from_utf8(value.to_vec()).unwrap())),
+        None => Ok(format!("Not data"))
+    }
+}
+
+#[get("/{key}/{value}")]
+async fn push(data: web::Data<sled::Db>, web::Path((key, value)): web::Path<(String, String)>) -> Result<String> {
+    match data.insert(key.as_bytes(), value.as_bytes()) {
+        Ok(_) => Ok(String::from("Value write")),
+        Err(e) => Ok(format!("Error: {}", e))
+    }
+}
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     //std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     let my_data = web::Data::new(sled::open("my_db")?);
 
     println!("Server up!");
 
-
     HttpServer::new(move || App::new()
             .wrap(middleware::Logger::default())
-            .app_data(my_data.clone()))
+            .app_data(my_data.clone())
+            .service(get)
+            .service(push)
+        )
         .bind("127.0.0.1:8080")?
         .run()
         .await
